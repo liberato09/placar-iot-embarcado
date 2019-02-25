846a5abd6ba7742e144196e99c2ca4f6f62e7a31
Lucas Vinicius ribeiro <lucasvribeiro14@gmail.com>
2019-02-25 17:44:20 -0300

    components

---
+++ reactjs/huntweb2/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import './styles.css';
+
+
+const  App = () => {
+    return (
+      <div className="App">
+        <Header /> 
+      </div>
+        
+    );
+}
+
+
+export default App;
